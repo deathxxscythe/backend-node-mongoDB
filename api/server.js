378ce73cad3125893fcca03c50ed54fd84e903ae
Dionne Stratton <68926102+Dionne-Stratton@users.noreply.github.com>
2022-05-05 13:55:18 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 
 const requireAuth = require('./middleware/requireAuth')
 const authRouter = require("./routes/authRoutes");
-const trackRouter = require("./routes/trackRoutes");
 
 const server = express();
 
@@ -12,7 +11,6 @@ server.use(express.json());
 server.use(cors());
 
 server.use("/api/auth", authRouter);
-server.use("/api/tracks", trackRouter);
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
