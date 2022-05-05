c1e0852586453ffc4be57f9f83ed26a10cf14365
Dionne Stratton <68926102+Dionne-Stratton@users.noreply.github.com>
2022-05-05 13:53:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+require('./api/models/User')
+require('./api/models/Track')
+const server = require('./api/server')
+
+const mongoose = require('mongoose')
+
+const URI = process.env.MONGODB_URL;
+const PORT = process.env.PORT || 9000
+
+mongoose.connect(URI, {
+    useNewUrlParser: true
+})
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo db')
+})
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo db', err)
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT} `)
+})
